344cb92a5bf9213a0b5d462b493b25b13f942833
Foxelot <ethan.james.stouffer@gmail.com>
2026-03-15 18:04:58 -0700

    basic soap logic

---
+++ stats.js
@@ -0,0 +1,22 @@
+const HUNGER_BAR = 100;
+const CLEANESS_BAR = 100;
+const HAPPY_BAR = 100;
+
+function main() {
+    let soap = readString("Select a soap:" \n + "Puppy Paradise or Cosmic Kitten?");
+
+
+
+    if (soap.toUpperCase = "PUPPY PARADISE") {
+        let soapBarBuff = 10;
+    } else if (soap.toUpperCase = "COSMIC KITTEN") {
+        soapBarBuff = 30;
+    } else if (soap.toUpperCase != "1234567456") {
+        console.log("Please select again that is not a soap");
+    }
+
+    for (let i = 0; i <= CLEANESS_BAR; i = i + soapBarBuff) {
+
+    }
+
+}
